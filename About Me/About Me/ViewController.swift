d733c2896772644f151d98d8aa7149c767d1164e
Jake Covert <jcovert7@ford.com>
2018-06-12 21:20:50 -0400

    Done.

---
+++ About Me/About Me/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  About Me
+//
+//  Created by Covert, Jake (J.) on 6/12/18.
+//  Copyright © 2018 Woodward Pet Sitting. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var infoView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func introduceMe(_ sender: UIButton) {
+        
+        let firstName:String = "Jake"
+        let lastName:String = "Covert"
+        let fullName:String = firstName + " " + lastName
+        let homeTown:String = "Omaha"
+        let homeState:String = "NE"
+        
+        // Tastes change, just not in this app...
+        var favFood:String = "Indian"
+        var favColor:String = "green"
+        
+        // Print out the info
+        print("Full name: \(fullName)")
+        print("Home Town: \(homeTown), \(homeState)")
+        print("Favorite Food: \(favFood)")
+        print("Favorite Color: \(favColor)")
+        
+        infoView.text = "Full name: \(fullName)\n Home Town: \(homeTown), \(homeState) \n Favorite Food: \(favFood) \n Favorite Color: \(favColor)"
+    }
+    
+
+    
+    
+}
+
